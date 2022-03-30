fd06fb06284ed22c9bcfebe38ca335573c289e4f
harshithpabbati <pabbatiharshith@gmail.com>
2022-03-30 17:28:06 +0530

    fix error with sharedstate

---
+++ pages/api/transcription.js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    console.log(`Creating room on domain ${process.env.DAILY_DOMAIN}`);
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.DAILY_API_KEY}`,
+      },
+    };
+
+    const dailyRes = await fetch(
+      `${process.env.DAILY_REST_DOMAIN || 'https://api.daily.co/v1'}/`,
+      options
+    );
+
+    const { config, error } = await dailyRes.json();
+
+    if (error) {
+      return res.status(500).json({ error });
+    }
+
+    return res
+      .status(200)
+      .json({ isTranscriptionEnabled: !!config.enable_transcription });
+  }
+
+  return res.status(500);
+}
